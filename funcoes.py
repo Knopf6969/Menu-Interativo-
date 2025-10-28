f1e7104186ea9b5d2ed7c4afb04a0389530a9595
Artthur19 <arttaugusto51@gmail.com>
2025-10-28 20:15:03 -0300

    Estrutura inicial das funções

---
+++ funcoes.py
@@ -0,0 +1,12 @@
+import os 
+from datetime import datetime
+
+# Usaremos chaves curtas e padronizadas para facilitar
+CHAVES_TREINO = [
+    "dia_semana",
+    "grupo_muscular",
+    "exercicios",
+    "series",
+    "repeticoes",
+    "observacao"
+]
